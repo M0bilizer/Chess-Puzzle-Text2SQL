489f997ad99bcafb7c9dae8fbffc4b1a9cb4492f
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2024-10-17 20:45:50 +0800

    feat: SQL and LLM textbox

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -17,54 +17,118 @@ type puzzleType = {
 };
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState('');
+  const [SQLValue, setSQLValue] = useState('');
+  const [LLMValue, setLLMValue] = useState('');
   const [puzzles, setPuzzles] = useState([]);
+  const [LLMResponse, setLLMResponse] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSQLSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
       const res = await fetch('http://localhost:8080/api/query', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({query: inputValue})
+        body: JSON.stringify({ query: SQLValue }),
       });
 
       if (!res.ok) {
-        setInputValue('');
-        setError("Network response was not ok")
+        setSQLValue('');
+        setError('Network response was not ok');
         throw new Error('Network response was not ok');
       }
 
-      setError('')
+      setError('');
       const data = await res.json();
       setPuzzles(data);
     } catch (error: unknown) {
-
       if (error instanceof TypeError) {
         console.error('Network Error:', error);
-        setError('Could not recieve response');
+        setError('Could not receive response');
       }
 
       console.error('Error:', error);
     }
 
-    setInputValue('');
+    setSQLValue('');
+  };
+
+  const handleLLMSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:8080/api/llm', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: LLMValue }),
+      });
+
+      if (!res.ok) {
+        setLLMValue('');
+        setError('Network response was not ok');
+        throw new Error('Network response was not ok');
+      }
+
+      setError('');
+      const data = await res.json();
+      setLLMResponse(data);
+    } catch (error: unknown) {
+      if (error instanceof TypeError) {
+        console.error('Network Error:', error);
+        setError('Could not receive response');
+      }
+
+      console.error('Error:', error);
+    }
+
+    setLLMValue('');
   };
 
   return (
     <div className="w-1/3 h-full ml-auto mr-auto pl-4 rounded-lg flex flex-col items-center pt-40 gap-4">
       <h1>Chess Puzzle</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-        <input type="text" value={inputValue} className="border" onChange={(e) => setInputValue(e.target.value)} />
-        <button className="border rounded" type="submit">
-          Submit
-        </button>
-      </form>
+      <div className="flex border-gray-200 border p-4 gap-4">
+        <form onSubmit={handleSQLSubmit} className="flex flex-col gap-5">
+          <label htmlFor="sql">SQL</label>
+          <input
+            id="sql"
+            type="text"
+            value={SQLValue}
+            className="border"
+            onChange={(e) => setSQLValue(e.target.value)}
+          />
+          <button className="border rounded" type="submit">
+            Submit
+          </button>
+        </form>
+        <div className="border border-gray-200 p-2 flex flex-col">
+          Result
+          {puzzles && puzzles.map((puzzle: puzzleType) => <li key={puzzle.id}>{puzzle.puzzleId}</li>)}
+        </div>
+      </div>
+
+      <div className="flex border-gray-200 border p-4 gap-4">
+        <form onSubmit={handleLLMSubmit} className="flex flex-col gap-5">
+          <label htmlFor="LLM">LLM</label>
+          <input
+            id="llm"
+            type="text"
+            value={LLMValue}
+            className="border"
+            onChange={(e) => setLLMValue(e.target.value)}
+          />
+          <button className="border rounded" type="submit">
+            Submit
+          </button>
+        </form>
+        <p className="border border-gray-200 p-2 flex flex-col">
+          Result
+          {LLMResponse}
+        </p>
+      </div>
 
       {error.length > 0 && <div className="bg-red-400 rounded py-4 px-12">{error}</div>}
-      {puzzles && puzzles.map((puzzle: puzzleType) => <li key={puzzle.id}>{puzzle.puzzleId}</li>)}
     </div>
   );
 }
