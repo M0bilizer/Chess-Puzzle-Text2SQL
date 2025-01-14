52b897652314642be0be474035d43261f3df1324
LAPTOP-CKI2N8TA\Raki <rcaballer2-c@my.cityu.edu.hk>
2025-01-14 15:05:36 +0800

    fix: more fix for title

--- sveltekit/src/lib/components/Title.svelte
+++ sveltekit/src/lib/components/Title.svelte
@@ -2,11 +2,10 @@
 	import Fa6SolidChess from 'virtual:icons/fa6-solid/chess';
 </script>
 
-	<div class="flex flex-col items-center justify-center gap-0">
-		<Fa6SolidChess class="size-6"/>
-		<span class="text-xs tracking-tighter font-bold">Text2SQL</span>
-	</div>
+<div class="flex flex-col items-center justify-center gap-0 cursor-default select-none">
+	<Fa6SolidChess class="size-6"/>
+	<span class="text-xs tracking-tighter font-bold">Text2SQL</span>
+</div>
 
 <style>
-
 </style>
