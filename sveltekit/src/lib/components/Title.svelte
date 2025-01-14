461be42e4fb82578664ecccefc9b1266a88c7e9e
LAPTOP-CKI2N8TA\Raki <rcaballer2-c@my.cityu.edu.hk>
2025-01-14 14:42:37 +0800

    feat: Title

---
+++ sveltekit/src/lib/components/Title.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Fa6SolidChess from 'virtual:icons/fa6-solid/chess';
+</script>
+
+	<div class="flex flex-col items-center justify-center gap-0">
+		<Fa6SolidChess class="size-6"/>
+		<span class="text-xs tracking-tighter font-bold">Text2SQL</span>
+	</div>
+
+<style>
+
+</style>
--- sveltekit/src/lib/components/layout/Header.svelte
+++ sveltekit/src/lib/components/layout/Header.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import SearchModal from '$lib/components/modals/SearchModal.svelte';
+	import Title from '$lib/components/Title.svelte';
 </script>
 
 <header class="w-full border-b-[1px] border-surface-500/20 p-4 py-3 bg-surface-50-950 sm:px-10">
 	<div
 		class="container mx-auto grid max-w-screen-2xl grid-cols-[auto_1fr_auto] items-center gap-4 sm:grid-cols-[1fr_auto_1fr]"
 	>
-		<div class="left">hi hi</div>
+		<div class="left"><Title/></div>
 		<div class="middle"><SearchModal /></div>
 		<div class="right"></div>
 	</div>
