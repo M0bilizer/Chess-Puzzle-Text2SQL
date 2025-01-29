fae9683e5854bd3282c3f2c2ded4a8edca90b920
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2025-01-29 21:31:40 +0800

    fix: truncate bug

--- sveltekit/src/lib/components/layout/SearchRecord.svelte
+++ sveltekit/src/lib/components/layout/SearchRecord.svelte
@@ -12,7 +12,7 @@
 <button class="px-2 text-left text-sm hover:preset-tonal-primary" onclick={handleClick}>
 	<span class="flex flex-row justify-between">
 		<span class="truncate text-surface-500">{key}</span>
-		<span>
+		<span class="flex flex-row gap-1">
 			<SearchRecordToolTip {key} type={'won'} />
 			<SearchRecordToolTip {key} type={'notWon'} />
 		</span>
