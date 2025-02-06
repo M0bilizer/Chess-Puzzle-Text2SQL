13212bd5430bbd521672f0e5ac79bd353c599010
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2025-02-06 17:22:40 +0800

    feat: skip puzzle

--- sveltekit/src/lib/components/layout/BurgerMenu.svelte
+++ sveltekit/src/lib/components/layout/BurgerMenu.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
 	import { Popover, Switch } from '@skeletonlabs/skeleton-svelte';
 	import Fa6SolidBars from 'virtual:icons/fa6-solid/bars';
-	import { currentGame } from '$lib/stores/currentGameStore';
+	import { currentGame, skipCurrentGame } from '$lib/stores/currentGameStore';
 
 	let puzzleId: string = $state('');
 	let openState: boolean = $state(false);
 	let flipped: boolean = $state(false);
+	let disabled: boolean = $state(false);
+
+	function handleSkipPuzzle() {
+		skipCurrentGame();
+	}
 
 	currentGame.subscribe((state) => {
 		puzzleId = state.list[state.index].puzzle.puzzleId;
+		disabled = state.game.hasWon;
 	});
 </script>
 
@@ -23,14 +29,21 @@
 	{#snippet content()}
 		<div class="flex flex-col gap-2">
 			<span class="flex items-center justify-around align-middle">
-				<h6 class="h6">Flip Board</h6>
+				<span class="h6">Flip Board</span>
 				<Switch name="example" bind:checked={flipped} />
 			</span>
 			<hr class="hr" />
+			<button
+				class="px-2 text-left text-sm hover:preset-tonal-primary"
+				onclick={() => handleSkipPuzzle()}
+				{disabled}
+			>
+				<span class="text-surface-500">Skip puzzle</span>
+			</button>
 			<a
 				href="https://lichess.org/training/{puzzleId}"
 				target="_blank"
-				class="px-2 text-left text-sm underline hover:preset-tonal-primary"
+				class="px-2 text-left text-sm text-surface-500 underline hover:preset-tonal-primary"
 			>
 				Open puzzle in Lichess
 			</a>
--- sveltekit/src/lib/stores/currentGameStore.ts
+++ sveltekit/src/lib/stores/currentGameStore.ts
@@ -1,6 +1,7 @@
 import { get, writable } from 'svelte/store';
 import type { GameProgress } from '$lib/types/gameProgress';
 import type { PuzzleInstance } from '$lib/types/puzzleInstance';
+import { playMove } from '$lib/utils/chessUtils';
 
 export interface currentGameState {
 	query: string;
@@ -60,5 +61,39 @@ export function clearCurrentGame() {
 			moveIndex: 0,
 			hasWon: false
 		}
+	});
+}
+
+// export function skipMove() {
+// 	currentGame.update((state) => {
+// 		let fen = state.game.fen
+// 		fen = playMove(fen, state.game.moves[state.game.moveIndex])
+// 		return {
+// 			...state,
+// 			game: {
+// 				...state.game,
+// 				fen: fen,
+// 				moveIndex: state.game.moveIndex + 1,
+// 				hasWon: state.game.moveIndex + 1 === state.game.moves.length - 1,
+// 			}
+// 		}
+// 	});
+// }
+
+export function skipCurrentGame() {
+	currentGame.update((state) => {
+		const puzzleInstance = state.list[state.index];
+		let fen = puzzleInstance.puzzle.fen;
+		puzzleInstance.progress.moves.forEach((value) => (fen = playMove(fen, value)));
+
+		return {
+			...state,
+			game: {
+				...state.game,
+				fen: fen,
+				moveIndex: state.game.moves.length - 1,
+				hasWon: true
+			}
+		};
 	});
 }
