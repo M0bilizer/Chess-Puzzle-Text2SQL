1dc854a820ca583d07b623e8408b1e61defc0f63
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2025-02-24 15:54:54 +0800

    feat: updated microservice docker

--- microservice/start.sh
+++ microservice/start.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-pip install uv
-uv venv venv
-source venv/bin/activate
-uv pip install -r /app/requirements.txt
+uv pip install --system -r /app/requirements.txt
 uvicorn main:app --host 0.0.0.0 --port 8000
