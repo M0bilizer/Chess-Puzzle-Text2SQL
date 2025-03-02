de29358d63a3ab330bdb020453ef0eab61d74b32
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2025-03-02 13:58:28 +0800

    feat: add feedback store

--- frontend/src/lib/components/ui/MobileNextButton.svelte
+++ frontend/src/lib/components/ui/MobileNextButton.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import Fa6SolidSquareCaretRightRight from 'virtual:icons/fa6-solid/square-caret-right';
 	import Fa6SolidChessKing from 'virtual:icons/fa6-solid/chess-king';
+	import Fa6SolidXMark from 'virtual:icons/fa6-solid/xmark';
+	import Fa6SolidCheck from 'virtual:icons/fa6-solid/check';
 	import { loadNextGame, saveGame } from '$lib/utils/storeUtils';
 	import { currentGame, isAllFinished } from '$lib/stores/currentGameStore';
 	import { congratulationModalState } from '$lib/stores/modalStore';
+	import { feedbackState, feedbackStore } from '$lib/stores/feedbackStore';
 
-	let hasWon = false;
-	let orientation: 'w' | 'b' = 'w';
-	currentGame.subscribe((state) => {
-		hasWon = state.game.hasWon;
-		orientation = state.game.orientation;
+	let currentState = $state(feedbackState.white);
+
+	feedbackStore.subscribe((state) => {
+		currentState = state;
 	});
 
 	function handleClick() {
@@ -25,29 +27,45 @@
 <div
 	class="card h-[105px] border-[1px] align-middle shadow-xl border-surface-200-800 preset-filled-surface-100-900"
 >
-	{#if hasWon}
+	{#if currentState === feedbackState.won}
 		<button
 			class="flex h-full w-full flex-row items-center gap-2 pl-2 preset-filled-primary-100-900 hover:preset-filled-primary-200-800"
-			on:click={() => handleClick()}
+			onclick={() => handleClick()}
 		>
 			<Fa6SolidSquareCaretRightRight class="size-16 text-tertiary-500" />
 			<span class="h3">Next Puzzle</span>
 		</button>
+	{:else if currentState === feedbackState.correct}
+		<div class="flex h-full flex-row items-center justify-center gap-2 align-middle">
+			<Fa6SolidCheck class="size-16 text-primary-500" />
+			<div class="flex flex-col">
+				<h3 class="h5 font-bold text-tertiary-50">Best move!</h3>
+				<span class="text-tertiary-400">Keep going...</span>
+			</div>
+		</div>
+	{:else if currentState === feedbackState.wrong}
+		<div class="flex h-full flex-row items-center justify-center gap-2 align-middle">
+			<Fa6SolidXMark class="size-16 text-error-500" />
+			<div class="flex flex-col">
+				<h3 class="h5 font-bold text-tertiary-50">That's not the move!</h3>
+				<span class="text-tertiary-400">Try something else.</span>
+			</div>
+		</div>
 	{:else}
 		<div
 			class="flex h-full w-full flex-row items-center gap-2 p-2 align-middle"
-			class:white={orientation === 'w'}
-			class:black={orientation === 'b'}
+			class:white={currentState === feedbackState.white}
+			class:black={currentState === feedbackState.black}
 		>
 			<Fa6SolidChessKing class="size-16 text-inherit"></Fa6SolidChessKing>
 			<div class="flex flex-col items-center">
 				<h3 class="h3 font-semibold text-inherit">Your Turn</h3>
 				<span
 					class="text-sm font-semibold text-surface-500"
-					class:white={orientation === 'w'}
-					class:black={orientation === 'b'}
+					class:white={currentState === feedbackState.white}
+					class:black={currentState === feedbackState.black}
 				>
-					Find the best move for {orientation === 'w' ? 'white' : 'black'}
+					Find the best move for {currentState === feedbackState.white ? 'white' : 'black'}
 				</span>
 			</div>
 		</div>
