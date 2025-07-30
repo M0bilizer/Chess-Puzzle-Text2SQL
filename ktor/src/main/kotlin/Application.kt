dfc8658a82100ce78f7ad66d063a06fdc96b4255
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2025-07-30 23:36:05 +0800

    fix: moved loading .env into Application.configureEnvironment()

--- ktor/src/main/kotlin/Application.kt
+++ ktor/src/main/kotlin/Application.kt
@@ -1,19 +1,18 @@
 package com.chesspuzzletext2sql
 
+import com.chesspuzzletext2sql.plugins.configureConfigLoader
 import com.chesspuzzletext2sql.plugins.configureDatabases
+import com.chesspuzzletext2sql.plugins.configureEnvironment
 import com.chesspuzzletext2sql.plugins.configureHTTP
 import com.chesspuzzletext2sql.plugins.configureKoin
-import com.chesspuzzletext2sql.plugins.configureLLMConfigurationManager
 import com.chesspuzzletext2sql.plugins.configureMonitoring
 import com.chesspuzzletext2sql.plugins.configureRouting
 import com.chesspuzzletext2sql.plugins.configureSerialization
-import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
 fun main() {
-    loadEnvIfExists()
     embeddedServer(
             CIO,
             port = 8080,
@@ -25,21 +24,12 @@ fun main() {
 }
 
 fun Application.module() {
+    configureEnvironment()
+    configureConfigLoader()
     configureKoin()
-    configureLLMConfigurationManager()
     configureSerialization()
     configureDatabases()
     configureMonitoring()
     configureHTTP()
     configureRouting()
-}
-
-private fun loadEnvIfExists() {
-    try {
-        val dotenv = dotenv()
-        dotenv.entries().forEach { entry -> System.setProperty(entry.key, entry.value) }
-        println("Loaded .env file")
-    } catch (_: Exception) {
-        println(".env file does not exist. Skipping loading environment variables from .env file.")
-    }
 }
