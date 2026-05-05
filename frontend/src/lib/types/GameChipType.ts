325f681b1c1bd09979240d35c8eb1dda9288a3aa
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2026-05-05 20:49:23 +0800

    chore: remove old frontend code

--- frontend/src/lib/types/GameChipType.ts
+++
@@ -1,5 +0,0 @@
-export enum GameChipType {
-	won,
-	progress,
-	locked
-}
--- frontend/src/lib/types/puzzleInstance.ts
+++
@@ -1,7 +0,0 @@
-import type { GameProgress } from '$lib/types/gameProgress';
-import type { Puzzle } from '$lib/types/puzzle';
-
-export type PuzzleInstance = {
-	puzzle: Puzzle;
-	progress: GameProgress;
-};
