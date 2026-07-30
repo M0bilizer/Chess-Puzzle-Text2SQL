f18ffd5f6c06cc3ee44b92dac4fd528b3208d220
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2026-07-30 21:03:15 +0800

    fix: try relative base

--- sv-router/vite.config.ts
+++ sv-router/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src')
 		}
 	},
-	base: import.meta.env.VITE_BASE_PATH ? `/${import.meta.env.VITE_BASE_PATH}/` : '/',
+	base: './',
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
