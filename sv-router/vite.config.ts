e5ad6678f87526fd86bfd61ea929ae1962796105
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2026-07-30 21:28:49 +0800

    fix: load env in vite.config.ts

--- sv-router/vite.config.ts
+++ sv-router/vite.config.ts
@@ -3,49 +3,53 @@ import tailwindcss from '@tailwindcss/vite';
 import { playwright } from '@vitest/browser-playwright';
 import * as path from 'node:path';
 import Icons from 'unplugin-icons/vite';
+import { loadEnv } from 'vite';
 import runtimeEnv from 'vite-plugin-runtime-env';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		svelte(),
-		Icons({
-			compiler: 'svelte'
-		}),
-		runtimeEnv()
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src')
-		}
-	},
-	base: process.env.VITE_BASE_PATH ? `/${process.env.VITE_BASE_PATH}/` : '/',
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [
+			tailwindcss(),
+			svelte(),
+			Icons({
+				compiler: 'svelte'
+			}),
+			runtimeEnv()
+		],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src')
 			}
-		]
-	}
+		},
+		base: env.VITE_BASE_PATH ? `/${env.VITE_BASE_PATH}/` : '/',
+		test: {
+			expect: { requireAssertions: true },
+			projects: [
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'client',
+						browser: {
+							enabled: true,
+							provider: playwright(),
+							instances: [{ browser: 'chromium', headless: true }]
+						},
+						include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+						exclude: ['src/lib/server/**']
+					}
+				},
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'server',
+						environment: 'node',
+						include: ['src/**/*.{test,spec}.{js,ts}'],
+						exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					}
+				}
+			]
+		}
+	};
 });
