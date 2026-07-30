74cf5bbf579f555191a879a521f866954b626555
AWESOMEVIVOBOOK\rakib <everynameistakenlikethisone@gmail.com>
2026-07-30 20:42:13 +0800

    fix: add missing BACKEND_URL to vite-env.d.ts

--- sv-router/vite-env.d.ts
+++ sv-router/vite-env.d.ts
@@ -1,4 +1,7 @@
 interface ImportMetaEnv {
+	// set the railway's internal network IPv4 here
+	// only used by Caddy
+	readonly BACKEND_URL: string;
 	// backend api
 	readonly VITE_API_URL: string;
 	// if this is hosted at rakichazcaballero/chess-puzzle-textsql, set this as "chess-puzzle-text2sql"
